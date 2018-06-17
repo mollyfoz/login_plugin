1e5c1d8d7c964eed16d20853f47beeec865443a0
Molly Foster <mollyrfoster@gmail.com>
2018-06-17 09:00:09 -0600

    add uninstall file

---
+++ uninstall.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Trigger this file on Plugin Uninstall
+* @package LoginPlugin
+*/
+
+//destroy execution if not recognized as a wordpress method
+if( !defined('WP_UNINSTALL_PLUGIN') ) {
+  die;
+}
